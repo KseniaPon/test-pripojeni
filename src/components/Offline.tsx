1b4fe87756c739e6b8143faccebc96bd245b04e2
Ksenia Ponomareva <ponomarevaks@gmail.com>
2024-05-07 15:50:28 +0200

    useeffect, clear

--- src/components/Offline.tsx
+++ src/components/Offline.tsx
@@ -1,13 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Offline = () => {
   const [isOnline, setIsOnline] = useState<boolean>(navigator.onLine);
+  
+  const handleOffline = () => {
+    setIsOnline(false)
+    console.log('handleOffline')
+  }
+
+  const handleOnline = () => {
+    setIsOnline(true)
+    console.log('handleOnline')
+  }
+
+  useEffect(
+    () => {
+    console.log('mounted')
+    window.addEventListener('offline', handleOffline)
+    window.addEventListener('online', handleOnline)
+    
+    return () => {
+      console.log('unmounted')
+      window.removeEventListener('offline', handleOffline)
+      window.removeEventListener('online', handleOnline)
+    }
+  }, []);
+
 
   if (isOnline) {
     return (
       <p>Všechno v pořádku</p>
     )
   }
+
+
 
   return (
     <p>Jste offline!</p>
